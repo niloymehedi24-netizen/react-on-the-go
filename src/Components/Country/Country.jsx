2b8e89cf2bbc228e47d8bb448c80b9866d0d66dc
hasanniloy122-rgb <hasanniloy122@gmail.com>
2026-03-15 20:31:57 +0600

    added css styling over the countries and tried some conditional rendering

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
+import './country.css';
 
 const Country = ({country}) => {
+
+    const handleVisited = () => {
+        console.log('button clicked');
+    }
     
     return (
-        <div>
+        <div className='country'>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common} </h3>
             <h4>Continent: {country.continents.continents}</h4>
-            <p>Area: {country.area.area}</p>
+            <p>Area: {country.area.area} {country.area.area > 100000 ? '(Big country)' : '(Small country)'}</p>
             <p>Population: {country.population.population} </p>
+            <button onClick={handleVisited}>Not visited</button>
 
         </div>
     );
 };
 
 export default Country;
+
+
+/**
+ * CSS in React
+ * 
+ * 1. Inline CSS (Style Object)
+ * 2. 
+ * 
+ */
